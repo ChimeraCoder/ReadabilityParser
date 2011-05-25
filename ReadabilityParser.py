344293a7f465cb69b8e0542e515684627b272d5d
Aditya Mukerjee <dev@chimeracoder.net>
2011-05-24 20:32:19 -0400

    whitespace_replacer() now allows periods (to delimit sentences)

--- ReadabilityParser.py
+++ ReadabilityParser.py
@@ -5,8 +5,14 @@ import re
 
 def whitespace_replacer(text):
     '''Replace all characters that are not letters in text with whitespace'''
-    regex = re.compile('[^A-Za-z ]')
+    
+    #This regex will remove punctiation (except periods), special characters, and numbers
+    #Future versions should distinguish between decimal points and periods
+    regex = re.compile('[^A-Za-z. ]')
     return regex.sub(" ", text)
+
+
+
 
 
 
